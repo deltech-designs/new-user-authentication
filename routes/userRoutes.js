b9aeb7d3493c1690acbe7a729e567d8a2dc92260
deltechdesigns <deltechdesigns@gmail.com>
2025-04-17 00:26:38 +0100

    adjust the routes for auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import authRoutes from './userRoutes.js';
 
 const router = Router();
-router.use('/', authRoutes); // Use the user routes under the /api path
+router.use('/auth', authRoutes); // Use the user routes under the /api path
 
 export default router;
